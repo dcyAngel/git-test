e88d92f8d933110b41c3d3d978940451f7d98aad
dcy <593126070@qq.com>
2021-12-13 14:25:05 +0800

    hot-fix commit1

--- src/main/java/cn/dcy/git/GitTest.java
+++ src/main/java/cn/dcy/git/GitTest.java
@@ -8,8 +8,8 @@ package cn.dcy.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
