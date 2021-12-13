440392bcda8415f6eeeee3a1c1534ed5f9b9abe0
dcy <593126070@qq.com>
2021-12-13 09:49:51 +0800

    first commit

---
+++ src/main/java/cn/dcy/git/GitTest.java
@@ -0,0 +1,13 @@
+package cn.dcy.git;
+
+/**
+ * @Classname GitTest
+ * @Description TODO
+ * @Date 2021/12/12 17:44
+ * @Created by dcy
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
